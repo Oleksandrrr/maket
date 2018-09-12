a8f9fd14264f4afdf8588dbc53c98736694808c5
O-leksandr <yurchenko.oleksandr.89@gmail.com>
2018-09-12 16:55:06 +0300

    first commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,6 @@
+//= ../../../bower_components/jquery/dist/jquery.js
+//= ../../../bower_components/popper.js/dist/umd/popper.js
+//= ../../../bower_components/bootstrap/js/dist/util.js
+//= ../../../bower_components/bootstrap/js/dist/collapse.js
+
+//= ../../../bower_components/bootstrap/dist/js/bootstrap.min.js
